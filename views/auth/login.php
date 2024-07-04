0cad4d959f36fc51981ffb8436cb1febedaa26e0
reyes1010 <92321108+reyes1010@users.noreply.github.com>
2024-07-04 11:35:15 -0600

    change styles login

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,26 +1,30 @@
 <div class="contenedor-app">
     <div class="imagen"></div>
     <div class="app">
+        <div class="formulario_styles">
+            <form class="formulario" method="POST" action="/">
+                <legend>LOGIN</legend>
+                <div class="campo">
+                    <label for="email">Email</label>
+                    <input type="email" required id="email" placeholder="Tu Email" name="email" />
+                </div>
 
-        <form class="formulario" method="POST" action="/">
-            <div class="campo">
-                <label for="email">Email</label>
-                <input type="email" required id="email" placeholder="Tu Email" name="email" />
-            </div>
+                <div class="campo">
+                    <label for="password">Password</label>
+                    <input type="password" required id="password" placeholder="Tu Password" name="password" />
+                </div>
 
-            <div class="campo">
-                <label for="password">Password</label>
-                <input type="password" required id="password" placeholder="Tu Password" name="password" />
-            </div>
+                <?php
+                include_once __DIR__ . "/../templates/alertas.php";
+                ?>
+                <div class="alinear-derecha">
 
-            <?php
-            include_once __DIR__ . "/../templates/alertas.php";
-            ?>
-            <div class="alinear-derecha">
+                    <input type="submit" class="boton" value="Iniciar Sesión">
 
-                <input type="submit" class="boton" value="Iniciar Sesión">
+                </div>
+            </form>
 
-            </div>
-        </form>
+        </div>
+
     </div>
 </div>
